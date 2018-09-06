7e9464ad7d2a4e06858f7afea4415631f3e9430c
Vanna Vu <vannavu5539@gmail.com>
2018-09-06 14:07:11 -0500

    j-j-j-jqueeeeeery

---
+++ js/script.js
@@ -0,0 +1,35 @@
+console.log("hello");
+
+$(document).ready(function(){
+
+  $("#zero .hmiddle.opacity").click(function() {
+    $("#zero.glyph.opacity").animate({
+      opacity: 0.1,
+    }, 1000, function() {
+    });
+  });
+
+  $(".vemph").one("click", function() {
+    $(".vemph .htop, .vemph .hbottom").height(1).css({
+    });
+    $(".vemph .vleft, .vemph .vright").width(26).css({
+    });
+    $("#zero.glyph.vemph .hmiddle").css({
+      'transform': 'rotate(90deg)'
+    });
+  });
+
+  $(".glyph.invert").click(function() {
+    $(this).animate({
+      backgroundColor: "#000000",
+    }, 1000, function() {
+    });
+    $("#zero.glyph.invert .hmiddle").animate({
+      backgroundColor: "#ffffff",
+    }, 1000, function() {
+    });
+  });
+
+
+
+});
