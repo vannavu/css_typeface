00862176332d84a19c9f249afcfe4a95bece37ad
Vanna Vu <vannavu5539@gmail.com>
2018-09-15 21:59:07 -0500

    on/off states

--- js/final_script.js
+++ js/final_script.js
@@ -1,33 +1,15 @@
 $(document).ready(function(){
 
   $(".button").hover(function() {
-      $(this).css({
-        'transform': 'scale(1.25)',
-        'transition': 'transform 0.6s'
-      });
-    }, function() {
-        $(this).css({
-          'transform': 'scale(1)',
-          'transition': 'transform 1s'
-        });
+    $(this).css({
+      'transform': 'scale(1.25)',
+      'transition': 'transform 0.6s'
     });
-
-  $(".button").click(function() {
-    if($(this).hasClass('clicked')) {
-      $(this).removeClass('clicked');
+    }, function() {
       $(this).css({
         'transform': 'scale(1)',
-        'transition': 'transform 1s',
-      })
-    }
-    else {
-      $(this).off("mousenter mouseleave");
-      $(this).addClass('clicked');
-      $(this).css({
-        'transform': 'scale(1.25)',
-        'transition': 'transform 1s',
+        'transition': 'transform 1s'
       });
-    }
   });
 
   $(".transformation_icons").hover(function() {
@@ -42,25 +24,56 @@ $(document).ready(function(){
         });
     });
 
+  var hamburger_on = false;
   $("#hamburger").click(function() {
-    $(".word .htop").css({
-      'border-top-left-radius': '4rem',
-      'border-top-right-radius': '4rem',
-      'border-bottom-left-radius': '0',
-      'border-bottom-right-radius': '0',
-      'transition': 'border-radius 6s'
-    });
-    $(".word .hmiddle").css({
-      'border-radius': '0',
-      'transition': 'border-radius 3s'
-    });
-    $(".word .hbottom").css({
-      'border-top-left-radius': '0',
-      'border-top-right-radius': '0',
-      'border-bottom-left-radius': '4rem',
-      'border-bottom-right-radius': '4rem',
-      'transition': 'border-radius 2s'
-    });
+    if (hamburger_on == false) {
+      hamburger_on = true;
+      $(this).off("mouseover mouseleave");
+      $(this).css({
+        '--stroke_color': 'var(--button_on)',
+        'transform': 'scale(1.25)',
+        'transition': 'transform 0.5s'
+      });
+      $(".word .htop").css({
+        'border-top-left-radius': '4rem',
+        'border-top-right-radius': '4rem',
+        'border-bottom-left-radius': '0',
+        'border-bottom-right-radius': '0',
+        'transition': 'border-radius 6s'
+      });
+      $(".word .hmiddle").css({
+        'border-radius': '0',
+        'transition': 'border-radius 3s'
+      });
+      $(".word .hbottom").css({
+        'border-top-left-radius': '0',
+        'border-top-right-radius': '0',
+        'border-bottom-left-radius': '4rem',
+        'border-bottom-right-radius': '4rem',
+        'transition': 'border-radius 2s'
+      });
+    }
+    else {
+      hamburger_on = false;
+      $(this).on("mouseover mouseleave");
+      $(this).css({
+        '--stroke_color': 'var(--button_off)',
+        'transform': 'scale(1)',
+        'transition': 'transform 0.5s'
+      });
+      if (sausage_on == true) {
+        $(".word .htop, .word .hmiddle, .word .hbottom").css({
+          'border-radius': '4rem',
+          'transition': 'border-radius 1.5s'
+        });
+      }
+      else {
+        $(".word .htop, .word .hmiddle, .word .hbottom").css({
+          'border-radius': '0',
+          'transition': 'border-radius 1.5s'
+        });
+      }
+    }
   });
 
   $("#cake").click(function() {
@@ -70,50 +83,202 @@ $(document).ready(function(){
     });
   });
 
+  var sausage_on = false;
   $("#sausage").click(function() {
-    $(".word .htop, .word .hmiddle, .word .hbottom").css({
-      'border-radius': '4rem',
-      'transition': 'border-radius 1.5s'
-    });
+    if (sausage_on == false) {
+      $(this).off("mouseover mouseleave");
+      $(this).css({
+        '--stroke_color': 'var(--button_on)',
+        'transform': 'scale(1.25)',
+        'transition': 'transform 0.5s'
+      });
+      sausage_on = true;
+      $(".word .htop, .word .hmiddle, .word .hbottom").css({
+        'border-radius': '4rem',
+        'transition': 'border-radius 1.5s'
+      });
+    }
+    else {
+      sausage_on = false;
+      $(this).on("mouseover mouseleave");
+      $(this).css({
+        '--stroke_color': 'var(--button_off)',
+        'transform': 'scale(1)',
+        'transition': 'transform 0.5s'
+      });
+      if (hamburger_on == true) {
+        $(".word .htop").css({
+          'border-top-left-radius': '4rem',
+          'border-top-right-radius': '4rem',
+          'border-bottom-left-radius': '0',
+          'border-bottom-right-radius': '0',
+          'transition': 'border-radius 6s'
+        });
+        $(".word .hmiddle").css({
+          'border-radius': '0',
+          'transition': 'border-radius 3s'
+        });
+        $(".word .hbottom").css({
+          'border-top-left-radius': '0',
+          'border-top-right-radius': '0',
+          'border-bottom-left-radius': '4rem',
+          'border-bottom-right-radius': '4rem',
+          'transition': 'border-radius 2s'
+        });
+      }
+      else {
+        $(".word .htop, .word .hmiddle, .word .hbottom").css({
+          'border-radius': '0',
+          'transition': 'border-radius 1.5s'
+        });
+      }
+    }
   });
 
+  var link_on = false;
   $("#link").click(function() {
-    $(".word .glyph").css({
-      margin: '0 -0.5px',
-      'transition': 'margin 0.5s, width 0.5s',
-    });
+    if (link_on == false) {
+      link_on = true;
+      $(this).off("mouseover mouseleave");
+      $(this).css({
+        '--stroke_color': 'var(--button_on)',
+        'transform': 'scale(1.25)',
+        'transition': 'transform 0.5s'
+      });
+      $(".word .glyph").css({
+        '--side_bearings': '0rem',
+        '--glyph_width': 'calc(100% / var(--num_char) - 2 * var(--side_bearings))',
+        margin: '0 -0.5px',
+        'transition': 'margin 0.25s ease-in, width 0.25s ease-in',
+      });
+      $(".wrapper").css({
+        width: '95%',
+        'transition': 'width 1s'
+      });
+    }
+    else {
+      link_on = false;
+      $(this).on("mouseover mouseleave");
+      $(this).css({
+        '--stroke_color': 'var(--button_off)',
+        'transform': 'scale(1)',
+        'transition': 'transform 0.5s'
+      });
+      $(".word .glyph").css({
+        '--side_bearings': '0.4rem',
+        '--glyph_width': 'calc(100% / var(--num_char) - 2 * var(--side_bearings))',
+        margin: '0 var(--side_bearings)',
+        'transition': 'margin 0.25s ease-in, width 0.25s ease-in',
+      });
+      $(".wrapper").css({
+        width: '75%',
+        'transition': 'width 1s'
+      });
+    }
   });
 
+  var magnet_on = false;
   $("#magnet").click(function() {
-    $(".word").css({
+    if (magnet_on == false) {
+      magnet_on = true;
+      $(this).off("mouseover mouseleave");
+      $(this).css({
+        '--stroke_color': 'var(--button_on)',
+        'transform': 'scale(1.25)',
+        'transition': 'transform 0.5s'
+      });
+      $(".word").css({
         margin: '0',
         'transition': 'margin 0.65s ease-in',
-    });
-    $(this).css({
-      "--stroke_color": 'silver',
-    });
+      });
+    }
+    else {
+      magnet_on = false;
+      $(this).on("mouseover mouseleave");
+      $(this).css({
+        '--stroke_color': 'var(--button_off)',
+        'transform': 'scale(1)',
+        'transition': 'transform 0.5s'
+      });
+      $(".word").css({
+        margin: '3rem 0',
+        'transition': 'margin 0.65s ease-in',
+      });
+    }
   });
 
+  var invert_on = false;
   $("#i.button").click(function() {
-    $(".word .glyph").animate({
-      backgroundColor: 'black',
-    }, 500, function() {
-    });
-    $(".word .htop, .word .hmiddle, .word .hbottom, .word .vleft, .word .vright, .word .vmiddle" ).animate({
-      backgroundColor: 'white',
-    }, 2750, function() {
-    });
+    if (invert_on == false) {
+      invert_on = true;
+      $(this).off("mouseover mouseleave");
+      $(this).css({
+        'transform': 'scale(1.25)',
+        'transition': 'transform 0.5s'
+      });
+      $(".word .glyph").animate({
+        backgroundColor: 'black',
+      }, 500, function() {
+      });
+      $(".word .htop, .word .hmiddle, .word .hbottom, .word .vleft, .word .vright, .word .vmiddle" ).animate({
+        backgroundColor: 'white',
+      }, 2750, function() {
+      });
+    }
+    else {
+      invert_on = false;
+      $(this).css({
+        'transform': 'scale(1)',
+        'transition': 'transform 0.5s'
+      });
+      $(".word .glyph").animate({
+        backgroundColor: 'white',
+      }, 500, function() {
+      });
+      $(".word .htop, .word .hmiddle, .word .hbottom, .word .vleft, .word .vright, .word .vmiddle" ).animate({
+        backgroundColor: 'black',
+      }, 2750, function() {
+      });
+    }
   });
 
-  $("#b.button").click(function() {
-    $(".wrapper, section, body").animate({
-      backgroundColor: 'black',
-    }, 1000, function() {
-    });
-    $(".word").find(".htop, .hmiddle, .hbottom, .vleft, .vmiddle, .vright").animate({
-      backgroundColor: 'white',
-    }, 1000, function() {
-    });
+  var background_color = 'white';
+  $("#b.button, #w.button").click(function() {
+    if (background_color == 'white') {
+      background_color = 'black';
+      $(this).off("mouseover mouseleave");
+      $(this).css({
+        'transform': 'scale(1.25)',
+        'transition': 'transform 0.5s'
+      });
+      $(".wrapper, section, body").animate({
+        backgroundColor: background_color,
+      }, 1000, function() {
+      });
+      $("#b.button").css({
+        'display': 'none',
+      });
+      $("#w.button").css({
+        'display': 'inline-block',
+      });
+    }
+    else {
+      background_color = 'white';
+      $(this).css({
+        'transform': 'scale(1)',
+        'transition': 'transform 0.5s'
+      });
+      $(".wrapper, section, body").animate({
+        backgroundColor: background_color,
+      }, 1000, function() {
+      });
+      $("#b.button").css({
+        'display': 'inline-block',
+      });
+      $("#w.button").css({
+        'display': 'none',
+      });
+    }
   });
 
   $(".horizontal #z .dot").click(function() {
